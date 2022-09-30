8c64e0799ede2662580a3cdf19ba5fb711203e22
Justin Ho <hochungyan@live.hk>
2022-09-30 20:57:37 +0100

    Test 1 completed -Complex Approah

--- test/airport.spec.js
+++ test/airport.spec.js
@@ -1,4 +1,4 @@
-const { assertEquals } = require('./testing-framework');
+const test = require('../test-framework.js');
 const Airport = require("../src/airport");
 
 let expected;
@@ -10,18 +10,17 @@ let airport;
 // Test 1 - is anything added to the basket?
 
 console.log(`============================`);
-console.log(`Test 1 - makes sure plane is not already landed at the airport`);
+console.log(`Test 1 - Airport#land()`);
 
-//Arrange
-airport = new Airport();
-airplane1 = {id: '1'};
-expected = 'This plane is already landed at the airport';
-
-//Act
-//Adding the first plane first
-airport.land(airplane1);
-
-//Adding the same plane again and expect it to return an error message
-actual = airport.land(airplane1);
-result = assertEquals(actual,expected);
-console.log(`Plane can not landed again if it is already in the airport: ${result}`);
+//
+test.it('cannot landed if the plane is already in the airport', function() {
+    // setup
+    airport = new Airport();
+    airplane1 = {id: '1'};
+    expected = 'This plane is already landed at the airport';
+    airport.land(airplane1);
+      // execute
+      let result = airport.land(airplane1);
+      // verify
+      test.assertEquals(result, "This plane is already landed at the airport");
+    });
